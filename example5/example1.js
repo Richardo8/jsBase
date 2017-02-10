ce5544f7336d6b701db3a6955f268801d0dc752b
LS <manunited_22@sina.com>
2017-02-10 16:56:12 +0800

    ES6的class与继承，多重继承

--- example5/example1.js
+++ example5/example1.js
@@ -306,7 +306,31 @@ class sonFooStatic extends FooStatic {
 
 console.log(sonFooStatic.classMethod());
 
+//Minxin模式的实现
+/*Mixin模式指的是，将多个类的接口“混入”（mix in）另一个类。它在ES6的实现如下。*/
+function minx(...minxins) {
+    class Mix{}
 
+    for(let minxin of minxins){
+       copyProperties(Mix, minxin);
+       copyProperties(Mix.prototype, minxin.prototype);
+    }
+
+    return Mix;
+}
+
+function copyProperties(target, source) {
+    for(let key of Reflect.ownKeys(source)){
+        if(key !== "constructor" && key !== "prototype" && key !== "name"){
+            let desc = Object.getOwnPropertyDescriptor(source, key);
+            Object.defineProperty(target, key, desc);
+        }
+    }
+}
+
+class DistributedEdit extends mix(Loggable, Serializable){
+
+}
 
 
 
