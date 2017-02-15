83d88098dc3f9b26dac6d72f8fae9e9a8825ec2d
LS <manunited_22@sina.com>
2017-02-15 10:32:58 +0800

    ES6 函数

--- example9/example1.js
+++ example9/example1.js
@@ -31,3 +31,29 @@ function createFunction1() {
 }
 
 console.log(createFunction1())
+
+
+
+
+
+function foo(x) {
+    var tmp = 3;
+    function bar(y) {
+        console.log((x + y + (++tmp)));
+    }
+    bar(10);
+}
+foo(2)
+/*这不是闭包。当你return的是内部function时，就是一个闭包。内部function会close-over外部function的变量直到内部function结束。*/
+
+
+
+
+
+
+
+
+
+
+
+
