44d939b4448d30d64103715c34694f62290883d2
LS <manunited_22@sina.com>
2017-04-12 16:03:38 +0800

    封装 混用的后果

--- OO/test.js
+++ OO/test.js
@@ -10,12 +10,18 @@ Book.prototype = {
     }
 }
 
-Book.prototype.display = function () {
+Book.prototype.display1 = function () {
     console.log('this is Book.prototype.display')
 }
 
+// Book.prototype = {
+//
+// }
 
 var book = new Book('1', 'abc', '10');
 console.log(book)
 console.log(book.display());
-// 同时使用以上两种方式会造成覆盖
+console.log(book.display1());
+// 同时使用以上两种方式会造成覆盖，如果先使用Book.prototype.display1 = function (){}这种方式，那么这种方式定义的方法将全部被覆盖，加入将Book.prototype = {}这个对象置为空，则所有方法都将消失
+
+
