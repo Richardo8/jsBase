a2ed40501d398e3c838452a93f3de306fb69e8e0
LS <manunited_22@sina.com>
2017-04-13 10:52:20 +0800

    封装 继承中的寄生式继承

--- OO/test.js
+++ OO/test.js
@@ -266,4 +266,14 @@ function createBook(obj) {
     // 通过原型继承方式创建新对象
     var o = new inheritObject(obj);
     //拓展新对象
+    o.getName = function () {
+        console.log(name);
+    }
+    return o;
 }
+
+var createbook = createBook(book1);
+console.log(createbook);
+createbook.getName();
+// 不能初始化属性
+
